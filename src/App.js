a42a3d91fb7d9b93f87fa169605ac684a839fbd1
github wubuchako <jfdev12@gmail.com>
2020-05-20 11:00:31 +0900

    create class components and functional componets  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
+// class App extends Component {
+//   render(){
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={()=> {console.log("I am clicked")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
 
-
-class App extends Component {
-  render(){
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={()=> {console.log("I am clicked")}}/>
-      </React.Fragment>
-    )
-  }
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+}
+const Cat = () => {
+  return <div>Me!</div>
 }
 
 export default App;
