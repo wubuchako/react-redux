9a16327c2dc36f36c88b3d04846bca16e2f6f521
github wubuchako <jfdev12@gmail.com>
2020-05-20 11:24:30 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -12,14 +12,39 @@ import React from 'react';
 // }
 
 const App = () => {
-  return <div>
-    <Cat />
-    <Cat />
-    <Cat />
-  </div>
+  const profiles = [
+    {
+      name:"Taro",
+      age:10
+    },
+    {
+      name:"Hanako",
+      age: 5
+    },
+    {
+      name:"NoName"
+    }
+  ]
+  return (
+    <div>
+    {
+      profiles.map((profile, index) =>{
+        return <User name={profile.name} age={profile.age} key={index} />
+      })
+    }
+   </div>
+  )
 }
-const Cat = () => {
-  return <div>Me!</div>
+const User = (props) => {
+  return (
+    <div>Hi, I am {props.name}, and {props.age} years old!</div>
+  )
+}
+
+User.defaultProps = {
+  age:1
 }
 
 export default App;
+
+
